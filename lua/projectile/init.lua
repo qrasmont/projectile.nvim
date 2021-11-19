e68e7f4decf744328d733d6a616e813d2f3f1fd4
Quentin Rasmont <qrasmont@gmail.com>
2021-11-19 20:30:13 +0100

    Add do_actions function

--- lua/projectile/init.lua
+++ lua/projectile/init.lua
@@ -18,6 +18,29 @@ local function get(path, on_result)
     }):start()
 end
 
+-- Run projectile do
+-- @param path: where to run
+-- @param actions: a set of the actions to perform
+-- @param on_result: callback function, call when job ends
+local function do_actions(path, actions, on_result)
+    -- Use cwd if no path is provided
+    if path == nil or path == '' then
+        path = vim.fn.getcwd()
+    end
+
+    -- append the do commands to action list
+    table.insert(actions, 1, 'do')
+
+    -- Excute projectile's get command
+    Job:new({
+        command = 'projectile',
+        args = actions,
+        cwd = path,
+        on_exit = on_result,
+    }):start()
+end
+
 return {
-    get = get
+    get = get,
+    do_actions = do_actions
 }
