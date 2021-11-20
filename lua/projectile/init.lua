84befc6e15e5682536c9c2266bced2b5b6d8f4e8
Quentin Rasmont <qrasmont@gmail.com>
2021-11-20 12:40:49 +0100

    Extract jobs to their own file

--- lua/projectile/init.lua
+++ lua/projectile/init.lua
@@ -1,48 +1,8 @@
-local Job = require('plenary.job')
 local ui = require('projectile.ui')
+local jobs = require('projectile.jobs')
 
 local cached_actions = {}
 local nbr_actions = 0
-
--- Run projectile get
--- @param path: where to run
--- @param on_result: function callback, called when job ends
-local function get(path, on_result)
-    -- Use cwd if no path is provided
-    if path == nil or path == '' then
-        path = vim.fn.getcwd()
-    end
-
-    -- Excute projectile's get command
-    Job:new({
-        command = 'projectile',
-        args = { 'get' },
-        cwd = path,
-        on_exit = on_result,
-    }):sync()
-end
-
--- Run projectile do
--- @param path: where to run
--- @param actions: a set of the actions to perform
--- @param on_result: callback function, call when job ends
-local function do_actions(path, actions, on_result)
-    -- Use cwd if no path is provided
-    if path == nil or path == '' then
-        path = vim.fn.getcwd()
-    end
-
-    -- append the do commands to action list
-    table.insert(actions, 1, 'do')
-
-    -- Excute projectile's get command
-    Job:new({
-        command = 'projectile',
-        args = actions,
-        cwd = path,
-        on_exit = on_result,
-    }):start()
-end
 
 local function select_actions_cb(job, exit_code)
     if exit_code ~= 0 then
@@ -62,7 +22,7 @@ end
 
 local function run(path)
     -- Entry point get the action list then run select_actions
-    get(path, select_actions_cb)
+    jobs.get(path, select_actions_cb)
 
     -- Create popup window for action selection
     ui.create_selector_prompt(nbr_actions)
@@ -73,6 +33,4 @@ end
 
 return {
     run = run,
-    get = get,
-    do_actions = do_actions
 }
---
+++ lua/projectile/jobs.lua
@@ -0,0 +1,46 @@
+local Job = require('plenary.job')
+
+-- Run projectile get
+-- @param path: where to run
+-- @param on_result: function callback, called when job ends
+local function get(path, on_result)
+    -- Use cwd if no path is provided
+    if path == nil or path == '' then
+        path = vim.fn.getcwd()
+    end
+
+    -- Excute projectile's get command
+    Job:new({
+        command = 'projectile',
+        args = { 'get' },
+        cwd = path,
+        on_exit = on_result,
+    }):sync()
+end
+
+-- Run projectile do
+-- @param path: where to run
+-- @param actions: a set of the actions to perform
+-- @param on_result: callback function, call when job ends
+local function do_actions(path, actions, on_result)
+    -- Use cwd if no path is provided
+    if path == nil or path == '' then
+        path = vim.fn.getcwd()
+    end
+
+    -- append the do commands to action list
+    table.insert(actions, 1, 'do')
+
+    -- Excute projectile's get command
+    Job:new({
+        command = 'projectile',
+        args = actions,
+        cwd = path,
+        on_exit = on_result,
+    }):start()
+end
+
+return  {
+    get = get,
+    do_actions = do_actions
+}
